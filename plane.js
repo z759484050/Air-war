8295c7fbc493581afc4cc91faf5657d0b47a68bc
此间的少年 <759484050@qq.com>
2021-01-12 20:40:54 +0800

    创建本方飞机

--- plane.js
+++ plane.js
@@ -4,7 +4,40 @@ var startDiv = document.getElementById('startDiv');
 var mainDiv = document.getElementById('mainDiv');
 
 
+/*2.声明创建飞机构造函数*/
+function Plane(x,y,width,height,imageSrc){
+	//我的飞机设置属性
+	this.planeX = x;
+	this.planeY = y;
+	this.planeWidth = width;
+	this.planeHeight = height;
 
+	this.init = function(){
+		this.imageNode = null;
+		this.imageNode = document.createElement('img');
+		this.imageNode.setAttribute('src',imageSrc);
+		// this.imageNode.src = imageSrc;
+		// this.imageNode.style.position = 'absolute';
+		this.imageNode.style.left = this.planeX+'px';
+		this.imageNode.style.top = this.planeY+'px';
+		// 追加到mainDiv盒子中
+		mainDiv.appendChild(this.imageNode);
+	}
+	// 调用初始化方法
+	this.init();
+
+}
+
+/*声明我方飞机的函数*/
+function OurPlane(x,y){
+	// call()函数用于调用Plan函数,它会将当前对象传到Plan函数中使用
+	Plane.call(this,x,y,66,80,'image/我的飞机.gif');
+
+}
+
+// 我方飞机函数创建
+var selfPlane = new OurPlane(127,480);
+console.log(mainDiv);
 
 /*1.当点击开始按钮的时候会执行的方法*/
 function begin(){
