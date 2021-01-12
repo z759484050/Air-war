ad5dd0e215ba62bf8349b38d7c0f9babf1f21397
此间的少年 <759484050@qq.com>
2021-01-12 20:54:19 +0800

    敌方飞机向下移动

--- plane.js
+++ plane.js
@@ -2,7 +2,7 @@
 var startDiv = document.getElementById('startDiv');
 // 获取游戏主界面元素
 var mainDiv = document.getElementById('mainDiv');
-
+var score = 0;
 
 /*2.声明创建飞机构造函数*/
 function Plane(x,y,width,height,imageSrc){
@@ -12,6 +12,9 @@ function Plane(x,y,width,height,imageSrc){
 	this.planeWidth = width;
 	this.planeHeight = height;
 
+	this.planeIsDie = false;
+
+	//创建图片的节点元素
 	this.init = function(){
 		this.imageNode = null;
 		// 创建img标签
@@ -26,6 +29,14 @@ function Plane(x,y,width,height,imageSrc){
 	}
 	// 调用初始化方法
 	this.init();
+
+	//敌机向下移动方法
+	this.planeMove = function(){
+		// console.log(this.imageNode.offsetTop+'px'); //一开始的值就是敌机的y坐标
+		this.imageNode.style.top = this.imageNode.offsetTop+2+'px';
+		// 100+5+5+5+5+5
+
+	}
 
 }
 
@@ -108,13 +119,18 @@ function random(min,max){ //1  5
 function Enemy(a,b,width,height,imageSrc){
 	var r = random(a,b); //23 274
 	// console.log(r);
-	Plane.call(this,r,50,width,height,imageSrc);
+	Plane.call(this,r,0,width,height,imageSrc);
 }
+
 
 
 var mark1 = 0; //标记什么时候开始创建敌机
 var mark2 = 0;  //标记什么时候创建什么类型的飞机
 var bgPositionY = 0;
+
+var enemys = new Array(); //创建敌机数组,存放不同类型的飞机
+
+
 
 // 声明定时器调用的循环方法
 function circulation(){
@@ -128,30 +144,56 @@ function circulation(){
 	};
 
 	//2.创建敌方飞机
-   // 
    mark1++;
    if (mark1 == 50) { //1000毫秒创建敌机
    	mark2++;
    	if (mark2%5==0) { //创建中型飞机
-   		new Enemy(23,274,46,60,'image/enemy3_fly_1.png');
+   	    var middleEnemy = new Enemy(23,274,46,60,'image/enemy3_fly_1.png');
+   	    //将中型飞机添加到敌机数组中
+   	    enemys.push(middleEnemy);
    	}
    	if (mark2%20==0) { //创建大型飞机
-
-   		new Enemy(55,210,110,164,'image/enemy2_fly_1.png');
+   		var bigEnemy = new Enemy(55,210,110,164,'image/enemy2_fly_1.png');
+   		enemys.push(bigEnemy);
    	}else{ //创建小型飞机
-   		new Enemy(50,200,34,24,'image/enemy1_fly_1.png');
+   		var smallEnemy = new Enemy(50,200,34,24,'image/enemy1_fly_1.png');
+   		enemys.push(smallEnemy);
    	}
    	mark1=0;
-   };
- 
+   }
+
+   //获取到敌方飞机数组的长度
+   var enemysLength = enemys.length;
+   for(var i = 0;i<enemysLength;i++){
+
+   		if (!enemys[i].planeIsDie) { //飞机没有死
+   			//移动敌方飞机
+   			enemys[i].planeMove();
+   		}
+
+   		//判断敌机是否移出边界
+   		if (enemys[i].imageNode.offsetTop+enemys[i].planeHeight/2>568) {
+   			//如果移出边界,则删除敌机
+   			mainDiv.removeChild(enemys[i].imageNode);
+
+   			//splice(start,delectCount) 
+   			//从start位置删除delectCount数量的元素,并且返回一个新的数组
+   			enemys.splice(i,1);
+   			enemysLength--;
+
+   		}
+
+   }
+
    
 
 
 
-
-
-
 }
+
+
+
+// console.log(enemys);
 
 /*1.当点击开始按钮的时候会执行的方法*/
 function begin(){
